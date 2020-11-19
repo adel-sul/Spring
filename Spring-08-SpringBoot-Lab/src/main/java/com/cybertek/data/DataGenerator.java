cd7b833a77506a58a819a22d9a23c1678471f6de
Adel Suleymanov <adel.sul1986@gmail.com>
2020-11-18 20:58:36 -0500

    User register form + fields binding

---
+++ Spring-08-SpringBoot-Lab/src/main/java/com/cybertek/data/DataGenerator.java
@@ -0,0 +1,60 @@
+package com.cybertek.data;
+
+import java.util.*;
+
+public class DataGenerator {
+
+    public static List<String> statesList() {
+        return Arrays.asList(
+                "Alaska",
+                "Alabama",
+                "Arkansas",
+                "Arizona",
+                "California",
+                "Colorado",
+                "Connecticut",
+                "Delaware",
+                "Florida",
+                "Georgia",
+                "Hawaii",
+                "Iowa",
+                "Idaho",
+                "Illinois",
+                "Indiana",
+                "Kansas",
+                "Kentucky",
+                "Louisiana",
+                "Massachusetts",
+                "Maryland",
+                "Maine",
+                "Michigan",
+                "Minnesota",
+                "Missouri",
+                "Mississippi",
+                "Montana",
+                "NorthCarolina",
+                "NorthDakota",
+                "Nebraska",
+                "NewHampshire",
+                "NewJersey",
+                "NewMexico",
+                "Nevada",
+                "NewYork",
+                "Ohio",
+                "Oklahoma",
+                "Oregon",
+                "Pennsylvania",
+                "RhodeIsland",
+                "SouthCarolina",
+                "SouthDakota",
+                "Tennessee",
+                "Texas",
+                "Utah",
+                "Virginia",
+                "Vermont",
+                "Washington",
+                "Wisconsin",
+                "WestVirginia",
+                "Wyoming");
+    }
+}
