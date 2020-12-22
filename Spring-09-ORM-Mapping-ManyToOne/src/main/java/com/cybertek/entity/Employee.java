a3f070714541d183eef1b50df6e25c35fdc920c8
Adel Suleymanov <adel.sul1986@gmail.com>
2020-12-21 22:10:28 -0500

    ManyToOne

---
+++ Spring-09-ORM-Mapping-ManyToOne/src/main/java/com/cybertek/entity/Employee.java
@@ -0,0 +1,48 @@
+package com.cybertek.entity;
+
+import com.cybertek.enums.Gender;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name= "employees")
+@NoArgsConstructor
+@Setter
+@Getter
+public class Employee extends BaseEntity{
+
+    public Employee(String firstName, String lastName, String email, LocalDate hireDate, Gender gender, int salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.hireDate = hireDate;
+        this.gender = gender;
+        this.salary = salary;
+    }
+
+    @Column(name = "name")
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate hireDate;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private int salary;
+
+    @ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST},fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id") // custom name for column
+    private Department department;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+}
