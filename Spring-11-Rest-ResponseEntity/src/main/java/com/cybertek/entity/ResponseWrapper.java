e1e4d0c6870c4c810eec929d9416ad68e411f776
Adel Suleymanov <adel.sul1986@gmail.com>
2021-02-16 21:37:24 -0500

    Spring REST - ResponseEntity

--- Spring-11-Rest-ResponseEntity/src/main/java/com/cybertek/entity/ResponseWrapper.java
+++ Spring-11-Rest-ResponseEntity/src/main/java/com/cybertek/entity/ResponseWrapper.java
@@ -1,5 +1,6 @@
 package com.cybertek.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 @NoArgsConstructor
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL) // if any of the fields is null, it will be ignored
 public class ResponseWrapper {
 
     private boolean success;
