ead528493bbeea5cf17cf3c5b065e7448add551c
Adel Suleymanov <adel.sul1986@gmail.com>
2021-03-07 21:10:52 -0500

    Spring Security - JWT

---
+++ Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/util/JWTUtil.java
@@ -0,0 +1,69 @@
+package com.cybertek.util;
+
+import com.cybertek.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class JWTUtil {
+
+    @Value("${security.jwt.secret-key}") // reads from property file
+    private String secret;
+
+    public String generateToken(User user, String userName) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("userName", user.getUserName());
+        claims.put("email", user.getEmail());
+
+        return createToken(claims, userName);
+    }
+
+    private String createToken(Map<String, Object> claims, String userName) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userName)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 *10)) //10 hours token validity
+                .signWith(SignatureAlgorithm.ES256, secret).compact();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private <T> T extraClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String extractUserName(String token) {
+        return extraClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extraClaim(token, Claims::getExpiration);
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String userName = extractUserName(token);
+        return userName.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+
+}
